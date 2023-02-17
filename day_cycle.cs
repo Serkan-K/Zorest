bee8e375b5980e5625c4797d8cd98137fafe3e00
Serkan <125659165+Serkan-K@users.noreply.github.com>
2023-02-17 16:17:22 +0300

    Create day_cycle.cs

---
+++ day_cycle.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunDongusu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(new Vector3(-500, 0, 0f), Vector3.right, 1f * Time.deltaTime);
+    }
+}
